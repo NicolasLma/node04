1720a82229a94fc4756649e7a604a558b850df00
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-04-11 12:15:06 +0200

    finished node04

---
+++ index.js
@@ -0,0 +1,8 @@
+// server.js
+require("dotenv").config(); // this line is mandatory when you want to read variables from the '.env' file
+
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}`);
